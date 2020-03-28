cdab1bc72e6ae0d54a3b9fb19a12cabf7f3c393b
DryMemoG <memo29sep@gmail.com>
2020-03-27 23:51:06 -0600

    SELECT
    recognize a select in normal language in spanish like "Selecciona todo de la tabla Humano" and  converts into a "SELECT * FROM Humano

--- speechrecognition.py
+++ speechrecognition.py
@@ -2,12 +2,34 @@ import speech_recognition as SR
 
 R = SR.Recognizer()
 
+
 with SR.Microphone() as source:
     print("Di algo: ")
     audio = R.listen(source)
 
     try:
         text = R.recognize_google(audio, language='es-MX')
-        print("Has dicho: {}".format(text))
+        #print("Has dicho: {}".format(text))
+        arrayS = text.split(' ')
+        
+        if arrayS[0]=="selecciona" or arrayS[0]=="seleccionar":
+            array2=["","","",""]
+            array2[0] = "SELECT"
+            if arrayS[1] =="todos" or arrayS[1]=="todo":
+                array2[1] = "*"
+            if arrayS[2]=="de" and arrayS[3]=="la" or arrayS[3]=="las":
+                array2[2]= "FROM"
+                array2[3]=arrayS[5]
+            for i in range(0,4):
+                print(array2[i],end=" ") 
+            print()
+        else:
+            print("No es un Select")
+        
+        
     except:
         print("No te entiendo")
+
+
+
+
