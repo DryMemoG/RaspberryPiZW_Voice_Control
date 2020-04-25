a0843bbecc3f7faf5235c59ef296e90ebcd94b09
DryMemoG <memo29sep@gmail.com>
2020-04-24 23:41:13 -0600

    MySQL Function  Added
    Add a new function to conect MySQL with pymysql and change the print of an Array to a string.

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,34 +1,46 @@
 import speech_recognition as SR
-
+import pymysql
 R = SR.Recognizer()
 
 
 with SR.Microphone() as source:
     print("Di algo: ")
     audio = R.listen(source)
-
+    sQuery=""
     try:
         text = R.recognize_google(audio, language='es-MX')
         #print("Has dicho: {}".format(text))
         arrayS = text.split(' ')
-        
+        #iniciamos con identificar un SELECT.
         if arrayS[0]=="selecciona" or arrayS[0]=="seleccionar":
             array2=["","","",""]
-            array2[0] = "SELECT"
+            array2[0] = "SELECT "
             if arrayS[1] =="todos" or arrayS[1]=="todo":
-                array2[1] = "*"
+                array2[1] = "* "
             if arrayS[2]=="de" and arrayS[3]=="la" or arrayS[3]=="las":
-                array2[2]= "FROM"
+                array2[2]= "FROM "
                 array2[3]=arrayS[5]
             for i in range(0,4):
-                print(array2[i],end=" ") 
-            print()
+                sQuery=sQuery+array2[i]
+            print(sQuery)
         else:
-            print("No es un Select")
+            print("Select mal Redactado amiguito, Habla claro.")
         
         
     except:
         print("No te entiendo")
+
+
+def mySql_Search(sQuery):
+    db = pymysql.connect("localhost","root", "","dbRaspberry")
+    cursor = db.cursor()
+    cursor.execute(sQuery)
+    results = cursor.fetchall()
+    for row in results:
+        id = row[0]
+        
+
+
 
 
 
