b2a4f56ff4d84caf3aa83df68702f585a8a77ebf
DryMemoG <memo29sep@gmail.com>
2020-05-14 13:38:51 -0600

    Changed to Windows
    Builded to run in windows due Memory problems with the Library in RaspberryPi

--- speechrecognition.py
+++ speechrecognition.py
@@ -1,6 +1,20 @@
 import speech_recognition as SR
-import pymysql
 R = SR.Recognizer()
+import pymysql
+import serial, time
+import os
+
+
+#funcion de busqueda en MySql
+def MySQL_buscar(sQuery):
+    try:
+        db = pymysql.connect("localhost","root", "","dbRaspberry")
+        cursor = db.cursor()
+        cursor.execute(sQuery)
+        result = cursor.fetchall()
+        print(result)
+    except:
+        print("Problemas de conexión")
 
 
 with SR.Microphone() as source:
@@ -9,7 +23,7 @@ with SR.Microphone() as source:
     sQuery=""
     try:
         text = R.recognize_google(audio, language='es-MX')
-        #print("Has dicho: {}".format(text))
+        print("Has dicho: {}".format(text))
         arrayS = text.split(' ')
         #iniciamos con identificar un SELECT.
         if arrayS[0]=="selecciona" or arrayS[0]=="seleccionar":
@@ -20,28 +34,36 @@ with SR.Microphone() as source:
             if arrayS[2]=="de" and arrayS[3]=="la" or arrayS[3]=="las":
                 array2[2]= "FROM "
                 array2[3]=arrayS[5]
-            for i in range(0,4):
+            for i in range(0,len(array2)):
                 sQuery=sQuery+array2[i]
             print(sQuery)
+            MySQL_buscar(sQuery)
+            #un select con Like para buscar cualquier comida
+        elif arrayS[0]=="Busca" or arrayS[0]=="buscar":
+            array2=["SELECT ","* ","FROM ","comida ","WHERE ","nombrecomida ","LIKE ",""]
+            array2[7]="'%"+arrayS[1]+"%'"
+            for i in range(0,len(array2)):
+                sQuery=sQuery+array2[i]
+            print(sQuery)
+            MySQL_buscar(sQuery)
+            #envia un 1 via serial para encender el led en el programa arduino
+        elif arrayS[0]=="enciende" or arrayS[0]=="encender":
+            arduino=serial.Serial("COM3",9600)
+            time.sleep(2)
+            arduino.write(b'1')
+            arduino.close()
+            #envia un 0 via serial para apagar el led en el arduino
+        elif arrayS[0]=="apagar" or arrayS[0]=="apaga":
+            arduino=serial.Serial("COM3",9600)
+            time.sleep(2)
+            arduino.write(b'0')
+            arduino.close()
+            #abre cualquier programa de windows
+        elif arrayS[0]=="Abre":
+            os.system("NOTEPAD.EXE")
         else:
-            print("Select mal Redactado amiguito, Habla claro.")
+            print("Amiguito, Habla claro.!!!")
         
         
     except:
         print("No te entiendo")
-
-
-def mySql_Search(sQuery):
-    db = pymysql.connect("localhost","root", "","dbRaspberry")
-    cursor = db.cursor()
-    cursor.execute(sQuery)
-    results = cursor.fetchall()
-    for row in results:
-        id = row[0]
-        
-
-
-
-
-
-
